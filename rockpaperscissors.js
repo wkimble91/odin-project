e951e4e984eb59af946817fe8f40216fb107db83
Will <mail@willkimble.com>
2023-07-31 11:32:07 -0500

    added ui for best of 5

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,3 +1,15 @@
+const buttons = document.querySelectorAll('.btn');
+const currWinner = document.querySelector('.winner');
+const currScore = document.querySelector('.score');
+let playerScore = 0;
+let computerScore = 0;
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    playerSelection = button.id;
+    game(playerSelection);
+  });
+});
+
 let getComputerChoice = () => {
   let rockPaperScissors = ['Rock', 'Paper', 'Scissors'];
   return rockPaperScissors[Math.floor(Math.random() * 3)];
@@ -5,45 +17,49 @@ let getComputerChoice = () => {
 
 let playRound = (player, computer) => {
   if (player == computer) {
-    console.log('Player: ' + player + ', Computer: ' + computer + '. TIE!');
+    currWinner.textContent =
+      'Player: ' + player + ', Computer: ' + computer + '. TIE!';
   } else if (
     (player == 'Rock' && computer == 'Paper') ||
     (player == 'Scissors' && computer == 'Rock') ||
     (player == 'Paper' && computer == 'Scissors')
   ) {
-    console.log(
-      'Player: ' + player + ' Computer: ' + computer + '. Computer Wins!'
-    );
+    currWinner.textContent =
+      'Player: ' + player + ', Computer: ' + computer + '. Computer Wins!';
     return 'computerScored';
   } else if (
     (computer == 'Rock' && player == 'Paper') ||
     (computer == 'Scissors' && player == 'Rock') ||
     (computer == 'Paper' && player == 'Scissors')
   ) {
-    console.log('Player: ' + player + ' Computer: ' + computer + '. You Win!');
+    currWinner.textContent =
+      'Player: ' + player + ', Computer: ' + computer + '. You Win!';
     return 'playerScored';
   }
 };
 
-let game = () => {
-  let playerScore = 0;
-  let computerScore = 0;
+let game = (playerSelection) => {
   let round;
-  for (i = 0; i < 5; i++) {
-    const playerInitSelection = prompt('Choose Rock, Paper or Scissors: ');
-    const playerSelection =
-      playerInitSelection.charAt(0).toUpperCase() +
-      playerInitSelection.toLowerCase().slice(1);
-    const computerSelection = getComputerChoice();
-    round = playRound(playerSelection, computerSelection);
-    if (round == 'computerScored') {
-      computerScore++;
-    } else if (round == 'playerScored') {
-      playerScore++;
-    }
+  const computerSelection = getComputerChoice();
+  round = playRound(playerSelection, computerSelection);
+  if (round == 'computerScored') {
+    computerScore++;
+  } else if (round == 'playerScored') {
+    playerScore++;
   }
-  console.log(
-    'Final Score! Player: ' + playerScore + ' and Computer: ' + computerScore
-  );
+
+  if (playerScore >= 5) {
+    currWinner.textContent =
+      'You Win! Player: ' + playerScore + ', Computer: ' + computerScore + '.';
+    playerScore = 0;
+    computerScore = 0;
+  } else if (computerScore >= 5) {
+    currWinner.textContent =
+      'You Lose! Player: ' + playerScore + ', Computer: ' + computerScore + '.';
+    playerScore = 0;
+    computerScore = 0;
+  }
+
+  currScore.textContent =
+    'Player: ' + playerScore + ' and Computer: ' + computerScore;
 };
-game();
